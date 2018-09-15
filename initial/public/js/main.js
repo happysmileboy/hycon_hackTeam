4901881d8b264eaca109b352b4ec38cd358e257b
happysmileboy <t35114105@gmail.com>
2018-09-15 14:40:36 +0900

    force overide

--- initial/public/js/main.js
+++ initial/public/js/main.js
@@ -3,13 +3,34 @@ function uploadVideoData(){
 	var file = document.getElementById("file1").files[0];
 	console.log(file);
 	if(file) {
-		var hash = file.lastModified + file.size + timestamp;
-		$.get("/uploadVideoData?")
+		var name = Number(file.name.split("/").slice(-1).join().split(".").shift());
+		var hash = file.lastModified + file.size;
+		console.log(name, hash)
+		$.get("/uploadVideoData?name=" + name + "&hash=" + hash, function(data) {
+			console.log(data);
+			alert('page content:' + data);
+			// let transaction = transactions.filter(transaction => {
+			// 	return transaction.blockHash;
+			// })
+			$("#blockHash").text("하이");
+		})
 	} else {
 		alert("Please select a file");
 	}
 }
 
-function getVideoData(){
-
+function compareVideoData(){
+	console.log("hola");
+	var file = document.getElementById("file2").files[0];
+	console.log(file);
+	if (file){
+		var name = Number(file.name.split("/").slice(-1).join().split(".").shift());
+		var hash = file.lastModified + file.size;
+		console.log(name,hash);
+		var bol = $.get('/compareVideoData?name=' + name + "&hash=" + hash, function(data) {
+			alert('page content' + data);
+		});
+	} else {
+		alert("please select a file");
+	}
 }
