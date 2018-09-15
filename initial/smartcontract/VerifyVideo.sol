8c5d30c3711ac3b9c685df451c1afaac468b330a
happysmileboy <t35114105@gmail.com>
2018-09-15 18:01:18 +0900

    wow finished~~~~~

--- initial/smartcontract/VerifyVideo.sol
+++ initial/smartcontract/VerifyVideo.sol
@@ -75,8 +75,8 @@ contract VerifyVideo is Ownable {
         
     }
     
-    function uploadVideoData (uint256 _name, uint256 _videoHash) public /*onlyOwner*/ {
-        // require(nameToVideo[_name] == 0);  //In real contract It is necessary to create unique data, but for debug I delete
+    function uploadVideoData (uint256 _name, uint256 _videoHash) public onlyOwner {
+        require(nameToVideo[_name].name == 0);  //In real contract It is necessary to create unique data, but for debug I delete
 
         nameToVideo[_name] = Video(_name, _videoHash);
         videoList.push(nameToVideo[_name]);
